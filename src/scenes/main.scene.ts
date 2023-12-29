245e80803178d060ca0b55d68147e579b1555cc5
esteban-vm <estebanvm90@outlook.com>
2023-12-29 15:52:42 -0300

    make map nicer

--- src/scenes/main.scene.ts
+++ src/scenes/main.scene.ts
@@ -17,6 +17,7 @@ export default class Main extends Phaser.Scene {
     const map = this.make.tilemap({ key: 'map' })
     const tileset = map.addTilesetImage('rpg_nature_tileset', 'tiles', 32, 32, 0, 0)!
     map.createLayer('Capa de patrones 1', tileset, 0, 0)
+    map.createLayer('Capa de patrones 2', tileset, 0, 0)
     this.player = new Player(this, 0, 0)
     this.add.existing(this.player)
   }
