799403acc99ddba35398ef83d8eb610b597fa0a0
esteban-vm <estebanvm90@outlook.com>
2023-12-29 16:21:00 -0300

    added colliders

--- src/scenes/main.scene.ts
+++ src/scenes/main.scene.ts
@@ -16,9 +16,11 @@ export default class Main extends Phaser.Scene {
   create() {
     const map = this.make.tilemap({ key: 'map' })
     const tileset = map.addTilesetImage('rpg_nature_tileset', 'tiles', 32, 32, 0, 0)!
-    map.createLayer('Capa de patrones 1', tileset, 0, 0)
+    const layer1 = map.createLayer('Capa de patrones 1', tileset, 0, 0)!
     map.createLayer('Capa de patrones 2', tileset, 0, 0)
-    this.player = new Player(this, 0, 0)
+    layer1.setCollisionByProperty({ collides: true })
+    this.matter.world.convertTilemapLayer(layer1)
+    this.player = new Player(this, 100, 100)
     this.add.existing(this.player)
   }
 
