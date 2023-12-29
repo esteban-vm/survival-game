3466d6ffdf65b9cb3c3c15fcb3dbe647e73c1f26
esteban-vm <estebanvm90@outlook.com>
2023-12-29 12:43:22 -0300

    added tilemap

--- src/scenes/main.scene.ts
+++ src/scenes/main.scene.ts
@@ -9,9 +9,14 @@ export default class Main extends Phaser.Scene {
 
   preload() {
     Player.preload(this)
+    this.load.image('tiles', 'assets/rpg_nature_tileset.png')
+    this.load.tilemapTiledJSON('map', 'assets/map.json')
   }
 
   create() {
+    const map = this.make.tilemap({ key: 'map' })
+    const tileset = map.addTilesetImage('rpg_nature_tileset', 'tiles', 32, 32, 0, 0)!
+    map.createLayer('Capa de patrones 1', tileset, 0, 0)
     this.player = new Player(this, 0, 0)
     this.add.existing(this.player)
   }
